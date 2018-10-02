e1bbb6aafa9ed11ed16ca80e0f923bf150ea3a76
Piotr <petr_84@interia.pl>
2018-10-02 23:55:48 +0200

    run web driver with WebDriverManager

--- src/main/java/utilities/WebDriverUtils.java
+++ src/main/java/utilities/WebDriverUtils.java
@@ -1,6 +1,12 @@
 package utilities;
 
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Paths;
 import java.util.concurrent.TimeUnit;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -22,25 +28,29 @@ public class WebDriverUtils {
         boolean isDriverInitialized = false;
         try{
             if(nameWebBrowser.equals("Mozilla Firefox")){
-                System.setProperty("webdriver.gecko.driver", "src/main/resources/drivers/geckodriver-v0.20.1-win64/geckodriver.exe");
+                //System.setProperty("webdriver.gecko.driver", "src/main/resources/drivers/geckodriver-v0.20.1-win64/geckodriver.exe");
+                WebDriverManager.firefoxdriver().setup();
                 driver = new FirefoxDriver();
                 log.info("start run Mozilla Firefox browser");
                 isDriverInitialized = true;
             }
             if(nameWebBrowser.equals("Google Chrome")){
-                System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver_win32/chromedriver.exe");
+                //System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver_win32/chromedriver.exe");
+                WebDriverManager.chromedriver().setup();
                 driver = new ChromeDriver();
                 log.info("start run Google Chrome browser");
                 isDriverInitialized = true;
             }
             if(nameWebBrowser.equals("Internet Explorer")){
-                System.setProperty("webdriver.ie.driver", "src/main/resources/drivers/IEDriverServer_x64_3.9.0/IEDriverServer.exe");
+                //System.setProperty("webdriver.ie.driver", "src/main/resources/drivers/IEDriverServer_x64_3.9.0/IEDriverServer.exe");
+                WebDriverManager.iedriver().setup();
                 driver = new InternetExplorerDriver();
                 log.info("start run Internet Explorer browser");
                 isDriverInitialized = true;
             }
             if(nameWebBrowser.equals("Microsoft Edge")){
-                System.setProperty("webdriver.edge.driver", "src/main/resources/drivers/MicrosoftWebDriver.exe");
+                //System.setProperty("webdriver.edge.driver", "src/main/resources/drivers/MicrosoftWebDriver.exe");
+                WebDriverManager.edgedriver().setup();
                 driver = new EdgeDriver();
                 log.info("start run Microsoft Edge browser");
                 isDriverInitialized = true;
